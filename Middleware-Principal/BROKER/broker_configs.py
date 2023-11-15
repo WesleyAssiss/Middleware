63c42e1dc01ae088d3930c77b10d50613cddd652
Wesley Assis <88063740+WesleyAssiss@users.noreply.github.com>
2023-11-15 17:46:19 -0300

    Delete Middleware-Principal directory

--- Middleware-Principal/BROKER/broker_configs.py
+++
@@ -1,9 +0,0 @@
-broker_configs = {
-    "HOST": "localhost",
-    "PORT": 1883,
-    "KEPPALIVE": 60,
-    "TOPIC_SENSOR": "/sensor",
-    "TOPIC_ATUADOR": "/atuador",
-    "TOPIC_CONTROLADOR01": "/controlador01",
-    "TOPIC_CONTROLADOR02": "/controlador02"
-}
--- Middleware-Principal/MAIN/teste.py
+++
@@ -1,9 +0,0 @@
-from BANCO import connector
-
-db = connector.ConnectionDB()
-
-
-resp = db.select_many()
-
-for i in resp:
-    print(i)
